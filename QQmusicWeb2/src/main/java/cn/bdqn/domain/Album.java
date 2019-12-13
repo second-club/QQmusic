b0b62d5904927d932f8cc7b31731c5d6296fb4dd
Mr.song <m18838971864@163.com>
2019-12-13 09:06:16 +0800

    框架搭建

---
+++ QQmusicWeb2/src/main/java/cn/bdqn/domain/Album.java
@@ -0,0 +1,43 @@
+package cn.bdqn.domain;
+
+public class Album {
+    private Integer aId;
+
+    private String aName;
+
+    private Integer aSid;
+
+    private String aUrl;
+
+    public Integer getaId() {
+        return aId;
+    }
+
+    public void setaId(Integer aId) {
+        this.aId = aId;
+    }
+
+    public String getaName() {
+        return aName;
+    }
+
+    public void setaName(String aName) {
+        this.aName = aName == null ? null : aName.trim();
+    }
+
+    public Integer getaSid() {
+        return aSid;
+    }
+
+    public void setaSid(Integer aSid) {
+        this.aSid = aSid;
+    }
+
+    public String getaUrl() {
+        return aUrl;
+    }
+
+    public void setaUrl(String aUrl) {
+        this.aUrl = aUrl == null ? null : aUrl.trim();
+    }
+}
